adfc899ce943d2c7e9a0a03a53a77a0a49c13928
Thiago Rosa <thiagorosadasilva39@gmail.com>
2025-06-03 22:44:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-    "https://iaca-frontend.vercel.app"],
+    "https://iaca.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
