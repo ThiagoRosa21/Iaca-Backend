b92c79302bf5d14cb2ac59e0c194bd59bd61d73f
Thiago Rosa <thiagorosadasilva39@gmail.com>
2025-06-04 22:06:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://iaca.vercel.app",
-        "https://iaca-81sd3a2cs-thiago-rosas-projects-44c4046d.vercel.app",
+        "https://www.iaca.com.br",
+        "https://iaca.com.br",
+        "https://iaca-81sd3a2cs-thiago-rosas-projects-44c4046d.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
 create_db()
